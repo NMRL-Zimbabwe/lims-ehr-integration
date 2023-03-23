bb33f77c8f1980562c22e5d2ff508c64b13b7313
Culani <culanimsekiwa@yahoo.com>
2023-03-23 11:21:17 +0200

    added audit event classes

--- src/main/java/zw/org/nmrl/service/impl/ClientServiceImpl.java
+++ src/main/java/zw/org/nmrl/service/impl/ClientServiceImpl.java
@@ -2,6 +2,8 @@ package zw.org.nmrl.service.impl;
 
 import org.springframework.stereotype.Service;
 import zw.org.nmrl.domain.Client;
+import zw.org.nmrl.domain.Developer;
+import zw.org.nmrl.repository.ClientRepository;
 import zw.org.nmrl.service.ClientService;
 
 @Service
@@ -12,4 +14,10 @@ public class ClientServiceImpl implements ClientService {
         // TODO Auto-generated method stub
         return null;
     }
+
+    Client update(Client client) {
+        return null;
+    }
+
+    void delete(Client client) {}
 }
